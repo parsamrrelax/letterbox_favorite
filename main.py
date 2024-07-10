3b0281e5134c94cf841b3ec0af2478ea336cea73
Parsalp <parsa.osunlou1998@gmail.com>
2024-07-11 01:21:19 +0330

    main

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 from collections import defaultdict
 
-username = "parsamrrelax"
+username = input("Enter your username: ")
 url = f'https://letterboxd.com/{username}/following/'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Find all elements with the attribute 'data-username'
 followings = [tag['data-username'] for tag in soup.find_all(attrs={"data-username": True})]
 
-# Dictionary to count occurrences of each movie
 high_movie_counts = defaultdict(int)
 
 for following in followings:
@@ -32,15 +30,15 @@ for following in followings:
     for movie in half_star_movies:
         low_movie_counts[movie] += 1
 
-# Sort the movies by count in descending order
 sorted_movies = sorted(high_movie_counts.items(), key=lambda x: x[1], reverse=True)
 low_sorted_movies= sorted(low_movie_counts.items(), key=lambda x: x[1], reverse=True)
 
 
-# Save the sorted movie counts to a file
 with open('movie_counts.txt', 'w') as file:
     for movie, count in sorted_movies[:10]:
+        file.write('high ratings')
         file.write(f'{movie} : {count}\n')
     for movie, count in low_sorted_movies[:10]:
+        file.write('low ratings')
         file.write(f'{movie} : {count}\n')
         print(f'{movie} : {count}')
